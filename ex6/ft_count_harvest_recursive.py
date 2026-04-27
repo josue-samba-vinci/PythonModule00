0fd4003d95cc5c64b93e65058388dc3d3461b594
Josue Samba Dianzungu <josamba-@fu-r2-p8.s19.be>
2026-04-27 22:47:21 +0200

    add: ex1-2-3-4-5-6-7

---
+++ ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,12 @@
+def helper(n, day):
+    if (day <= n):
+        print('Day', day)
+        helper(n, day + 1)
+    else:
+        print('Harvest time!')
+
+
+def ft_count_harvest_recursive():
+    n = int(input('Days until harvest: '))
+    day = 1
+    helper(n, day)
